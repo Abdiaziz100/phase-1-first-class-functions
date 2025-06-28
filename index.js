6554955e1fa4d63219aa9f20b2c28c80ce4164bf
mohamed <abdiazizmahat@gmail.com>
2025-06-28 19:55:24 +0300

    PHASE-1-FIRST-CLASS-FUNCTION

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback) {
+  callback();
+}
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I'm a named function!");
+  }
+  return namedFunction;
+}
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("I'm an anonymous function!");
+  };
+}
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("I'm an anonymous function!");
+  };
+}
